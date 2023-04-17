8cde38a10ffc5d18f26ac4d0af0d91a1e76f41f4
Matthew Ingwersen <matthew@fly.io>
2023-04-17 13:05:16 -0400

    Make new machines an option for console --select

--- internal/command/console/console.go
+++ internal/command/console/console.go
@@ -140,7 +140,7 @@ func runConsole(ctx context.Context) error {
 
 func selectMachine(ctx context.Context, app *api.AppCompact, appConfig *appconfig.Config) (*api.Machine, bool, error) {
 	if flag.GetBool(ctx, "select") {
-		return promptForMachine(ctx)
+		return promptForMachine(ctx, app, appConfig)
 	} else if len(flag.Args(ctx)) == 1 {
 		return getMachineByID(ctx)
 	} else {
@@ -148,7 +148,7 @@ func selectMachine(ctx context.Context, app *api.AppCompact, appConfig *appconfi
 	}
 }
 
-func promptForMachine(ctx context.Context) (*api.Machine, bool, error) {
+func promptForMachine(ctx context.Context, app *api.AppCompact, appConfig *appconfig.Config) (*api.Machine, bool, error) {
 	if len(flag.Args(ctx)) != 0 {
 		return nil, false, errors.New("machine IDs can't be used with -s/--select")
 	}
@@ -165,7 +165,7 @@ func promptForMachine(ctx context.Context) (*api.Machine, bool, error) {
 		return nil, false, errors.New("no machines are available")
 	}
 
-	options := []string{}
+	options := []string{"create an ephemeral shared-cpu-1x machine"}
 	for _, machine := range machines {
 		options = append(options, fmt.Sprintf("%s: %s %s %s", machine.Region, machine.ID, machine.PrivateIP, machine.Name))
 	}
@@ -174,7 +174,11 @@ func promptForMachine(ctx context.Context) (*api.Machine, bool, error) {
 	if err := prompt.Select(ctx, &index, "Select a machine:", "", options...); err != nil {
 		return nil, false, fmt.Errorf("failed to prompt for a machine: %w", err)
 	}
-	return machines[index], false, nil
+	if index == 0 {
+		return makeEphemeralMachine(ctx, app, appConfig)
+	} else {
+		return machines[index-1], false, nil
+	}
 }
 
 func getMachineByID(ctx context.Context) (*api.Machine, bool, error) {
